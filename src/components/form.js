0ab30b34613fe2b33fd05790b2ed0029eff1944f
Uzoma <uzoma.nwanne@gmail.com>
2024-05-12 14:37:23 +0100

    form completed

--- src/components/form.js
+++ src/components/form.js
@@ -1,73 +1,141 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 // TODO: complete me
 
 const Form = () => {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
-  const [comments, setComments] = useState("");
-  const [isSubscribe, setIsSubscribe] = useState(false);
+  const [comment, setComment] = useState("");
+  const [isSubscribed, setIsSubscribed] = useState(false);
+  const [isDisabled, setIsDisabled] = useState(true);
+  const [success, setSuccess] = useState(false);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(iSubscribe);
-    alert(`Hi, my name is ${firstName}  ${lastName}`);
+    alert(`Hi, my name is ${firstName}  ${lastName} `);
+    let data;
+    if (isSubscribed) {
+      data = {
+        firstName,
+        lastName,
+        isSubscribed,
+        email,
+        comment,
+      };
+    } else {
+      data = { firstName, lastName, isSubscribed, comment };
+    }
+
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users",
+        {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        }
+      );
+      const json = await response.json();
+      setSuccess(true);
+      setTimeout(() => {
+        setSuccess(false);
+      }, 2000);
+      setFirstName("");
+      setLastName("");
+      setEmail("");
+      setComment("");
+      setIsSubscribed(current =>!current);
+      setIsDisabled(current => !current)
+    } catch (e) {
+      alert(e);
+    }
   };
+
+  useEffect(() => {
+    setIsDisabled(true);
+    if (firstName.length && lastName.length) {
+      setIsDisabled((isDisabled) => !isDisabled);
+    }
+  }, [firstName, lastName]);
 
   return (
     <>
+    {success  && <div className="p-4 border-white border-4 bg-green">Thanks for your submission {firstName}</div>}
       <form>
-        <div className="grid grid-rows-2 gap-2">
-          <div className="mt-2 mt-2 flex flex-col items-start">
-            <label htmlFor="firstName">First Name</label>
+        <div className="flex flex-col">
+          <div className="flex flex-col items-start">
+            <label htmlFor="firstName" className="text-white">
+              First Name
+            </label>
             <input
               name="firstName"
               id="firstName"
               type="text"
+              value={firstName}
               onChange={(e) => setFirstName(e.target.value)}
             />
           </div>
           <div className="mt-2 flex flex-col items-start">
-            <label htmlFor="lastName">Last Name</label>
+            <label htmlFor="lastName" className="text-white">
+              Last Name
+            </label>
             <input
               name="lastName"
               id="lastName"
               type="text"
+              value={lastName}
               onChange={(e) => setLastName(e.target.value)}
             />
           </div>
           <div className="mt-2 mt-2 flex flex-col items-start">
-            <label htmlFor="lastName">Comments</label>
+            <label htmlFor="lastName" className="text-white">
+              Comments
+            </label>
             <textarea
               name="comment"
               id="comments"
               type="text"
+              value={comment}
               className="resize mt-2"
-              onChange={(e) => setComments(e.target.value)}
+              onChange={(e) => setComment(e.target.value)}
             />
           </div>
           <div className="mt-2 flex flex-col items-start">
-            <label htmlFor="subscribe">Subscribe To Email</label>
+            <label htmlFor="subscribe" className="text-white">
+              Subscribe To Email
+            </label>
             <input
               name="subscribe"
               id="subscribe"
               type="checkbox"
-              value={true}
-              onChange={(e) => setIsSubscribe((current) => !current)}
+              value={isSubscribed}
+              onChange={(e) => setIsSubscribed((current) => !current)}
+              checked={isSubscribed}
             />
           </div>
-          {isSubscribe && (
-            <div className={(isSubscribe ? "block" : "hiddenss") + "mt-2 flex flex-col items-start"}>
-              <label htmlFor="email">Email</label>
+          {isSubscribed && (
+            <div className="mt-2 flex flex-col items-start">
+              <label htmlFor="email" className="text-white">
+                Email
+              </label>
               <input
                 name="email"
                 id="email"
                 type="text"
+                value={email}
                 onChange={(e) => setEmail(e.target.value)}
               />
             </div>
           )}
-          <button type="submit" onClick={handleSubmit} className="border-solid rounded-md border-white border-2">
+          <button
+            type="submit"
+            onClick={handleSubmit}
+            className="mt-4 border-solid rounded-md border-white border-2 text-white"
+            disabled={isDisabled}
+          >
             Submit
           </button>
         </div>
